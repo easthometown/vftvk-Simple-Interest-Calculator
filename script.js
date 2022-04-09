9b4f5d17abbcc79c982b2fcbb8b6ba419420b962
easthometown <93026188+easthometown@users.noreply.github.com>
2022-04-10 02:03:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ function compute()
     y = document.getElementById("years").value;
     i = p * y * r /100;
     var year = new Date().getFullYear()+parseInt(y);
-    
+    if(p<=0){
+        alert("Enter a positive number");
+         document.getElementById("principal").focus();
+        return false;
+    }
     document.getElementById("result").innerHTML="If you deposit <mark>"+p+"</mark>,\<br\>at an interest rate of<mark>"+r+"</mark>%\<br\>You will receive an amount of <mark>"+i+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
     
 }
