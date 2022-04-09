5f1fa5816262b5b6fc5a2ba3c2307d81c40e6ddb
easthometown <93026188+easthometown@users.noreply.github.com>
2022-04-10 01:21:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function compute()
    
    if(principal<=0){
       alert("Enter a positive number");
-      
-      return false;
+      principal.focus();
+     
    }else{
       document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>" 
    }
