f8485afc0635ee47fbb24db7504c4b1f509686c5
easthometown <93026188+easthometown@users.noreply.github.com>
2022-04-10 01:09:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ var year = new Date().getFullYear()+parseInt(years);
 
 function compute()
 {
-    
+   document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"; 
    if(principal<=0){
       alert("Enter a positive number");
       principal.focus();
       return false;
    }
-  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+  
        
 }
 
