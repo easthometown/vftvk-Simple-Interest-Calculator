6dcd2496f22b7fbea3d59c897a8220b087826f42
easthometown <93026188+easthometown@users.noreply.github.com>
2022-04-10 03:03:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var amount = interest+principal;
     
-    document.getElementById("result").innerHTML="If you deposit "+"\<mark\>"+principal+"\</mark\>+
-        ",\<br\>at an interest rate of "
+    document.getElementById("result").innerHTML="If you deposit "
+        +"\<mark\>"+principal+"\</mark\>+
+        ",\<br\>at an interest rate of "+
         "\<mark\>"+rate+"\</mark\>"+
-        "%\<br\>You will receive an amount of "
-        +interest+"\<br\>in the year "
+        "%\<br\>You will receive an amount of "+
+        "\<mark\>"+interest+"\</mark\>"+
+            "\<br\>in the year "
             +"\<mark\>"+year+"\</mark\>\<br\>";
     
 }
