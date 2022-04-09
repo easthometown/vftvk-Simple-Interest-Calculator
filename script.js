bc1628592817d109c18815fae7b20a8be0e92b26
easthometown <93026188+easthometown@users.noreply.github.com>
2022-04-09 23:45:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+
 function compute()
 {
     p = document.getElementById("principal").value;
