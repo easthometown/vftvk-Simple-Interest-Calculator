a10704e97a35b2a29a325b5d7282869d05eb0d0d
easthometown <93026188+easthometown@users.noreply.github.com>
2022-04-10 01:08:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function compute()
       principal.focus();
       return false;
    }
-  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+  document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
        
 }
 
