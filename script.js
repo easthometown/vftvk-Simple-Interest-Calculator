f1f1c6fd7d9e90a45c5869670bdb9fab013855ad
easthometown <93026188+easthometown@users.noreply.github.com>
2022-04-10 01:58:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-
 function compute()
 {
-   principal = document.getElementById("principal").value;
-   rate = document.getElementById("rate").value;
-   years = document.getElementById("years").value;
-   interest = principal * years * rate /100;
-   year = new Date().getFullYear()+parseInt(years);
-  
-   document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    i = p * y * r /100;
+    var year = new Date().getFullYear()+parseInt(y);
+    
+    document.getElementById("result").innerHTML="If you deposit <mark>"+p+"</mark>,\<br\>at an interest rate of<mark>"+r+"</mark>%\<br\>You will receive an amount of <mark>"+i+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+    
 }
-
-function updateRate() 
+    function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
+
+    
 }
 function myFunction() {
     let age = document.getElementById("principal").value;
